6e115c342b7a84264917dfdd30121e276ffa3f08
John Baker <john@the-bakers.com>
2013-11-15 10:45:10 -0600

    initial commit.  Builds a static universal libsqlcipher using openssl 1.0.1e and sqlcipher 2.2.1.

---
+++ SqlCipherLib/SqlCipherLib.h
@@ -0,0 +1,13 @@
+//
+//  SqlCipherLib.h
+//  SqlCipherLib
+//
+//  Created by John Baker on 11/15/13.
+//  Copyright (c) 2013 Servpro Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SqlCipherLib : NSObject
+
+@end
---
+++ SqlCipherLib/SqlCipherLib.m
@@ -0,0 +1,13 @@
+//
+//  SqlCipherLib.m
+//  SqlCipherLib
+//
+//  Created by John Baker on 11/15/13.
+//  Copyright (c) 2013 Servpro Industries. All rights reserved.
+//
+
+#import "SqlCipherLib.h"
+
+@implementation SqlCipherLib
+
+@end
